e099f75c74a612758d06a068870fa8f2e6971e0f
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2024-02-13 21:00:22 +0300

    Add state

---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""State Module for HBNB project.
+
+Defines the State class representing states in HBNB.
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """State class."""
+    name = ""
