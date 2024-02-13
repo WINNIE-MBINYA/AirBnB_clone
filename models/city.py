f53c827de7a08915331022160be317e194b2eb16
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2024-02-13 20:58:15 +0300

    Add city

---
+++ models/city.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" City Module for HBNB project 
+
+Defines the City class representing cities in HBNB.
+"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """ City class containing state id and name"""
+    state_id = ""
+    name = ""
