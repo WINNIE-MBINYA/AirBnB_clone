f653d6bcb128216c78b8412b81ec11080285c383
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2024-02-13 20:57:41 +0300

    Add place

---
+++ models/place.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Place Module for HBNB project 
+
+Defines the Place class representing a place to stay.
+"""
+from models.base_model import BaseModel
+
+
+class Place(BaseModel):
+    """Place class containing information about a place to stay."""
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
